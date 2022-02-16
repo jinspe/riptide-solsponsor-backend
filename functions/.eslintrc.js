1a757fe071cd3f354009f8dd4458154307ac4a65
JINDBS <jean.duquenne.ch@gmail.com>
2022-02-16 11:33:33 +0100

    first commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    es5: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "google"],
+  rules: {
+    quotes: ["error", "double"],
+  },
+};
