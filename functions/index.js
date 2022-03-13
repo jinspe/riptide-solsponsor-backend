cfa4b63aeea2f2a7e388fec3ada13144f00f151b
JINDBS <jean.duquenne.ch@gmail.com>
2022-03-14 00:46:18 +0100

    mvp version

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,10 @@
 const admin = require("firebase-admin");
 admin.initializeApp({ credential: admin.credential.applicationDefault() });
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
-exports.testFunction = require("./src/testFunction/testFunction");
-exports.solanaTrans = require("./src/testFunction/solanaTransaction");
 exports.authentication = require("./src/authentication/authentication");
 exports.createUserName = require("./src/userSettings/createUserName");
 exports.updateUserName = require("./src/userSettings/updateUserName");
--- functions/src/testFunction/testFunction.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-
-exports.testFunction = functions.https.onCall(async (data) => {
-  return {
-    // New user he can signin
-    info: "hello",
-    message: data,
-  };
-});
